be2d51693040fee755ed871fac4548066eb076e9
ChristopherNatale <cnatale1048@gmail.com>
2025-01-29 12:04:19 -0500

    Fixed the overflow of some boxes on smaller resolution sizes.

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,7 +1,11 @@
 import { useParams, Link } from "react-router-dom";
 import defaultUserImg from "../assets/defaultPFP.png";
+import { getAuthToken } from "../util/auth";
 
 export default function User() {
+
+    const token = getAuthToken();
+
     let params = useParams();
     console.log(params);
     return (
@@ -10,7 +14,8 @@ export default function User() {
     <img className="profilePicture" src={defaultUserImg}/>
     <div className="profileButtons"> 
     <Link to={`/demonlist/${params.username}`}> <button className="viewListBttn"> View Demon List </button> </Link>
-    <Link to={`/demonlist/compare/${params.username}`}> <button className="compareListBttn"> Compare Lists </button> </Link>
+    <Link to={`/demonlist/compare/${params.username}`}> {token ? <button className="compareListBttn"> Compare Lists </button> :
+     <button disabled className="compareListBttn"> Compare Lists </button>}</Link>
     </div>
     </div>
     );
