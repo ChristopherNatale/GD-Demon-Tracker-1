81a42d35f5108400488b41cc31512bcc78a34c42
ChristopherNatale <cnatale1048@gmail.com>
2025-02-01 19:39:45 -0500

    Visual improvements.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,13 +6,15 @@ export default function Home() {
     <>
     <Header/>
     <div className="aboutContainer"> 
-    <h2> About </h2>
-    <p> While Geometry Dash allows users to keep track of their own level completions, it does NOT have 
-        the functionality for users to compare or even view the completions of others. This project aims to fix this problem
-        by serving as a hub whereby friends can share their demon completions (and progress!) with ease. 
-        Create an account now  to form your own demon list and join in on the fun!
-        <Link className="inlineLink" to="/signup"> Sign Up </Link>
-    </p>
+        <div className="aboutItem"> 
+            <h2> About </h2>
+            <p> While Geometry Dash allows users to keep track of their own level completions, it does NOT have 
+                the functionality for users to compare or even view the completions of others. This project aims to fix this problem
+                by serving as a hub whereby friends can share their demon completions (and progress!) with ease. 
+                Create an account now  to form your own demon list and join in on the fun!
+            <Link className="inlineLink" to="/signup"> Sign Up </Link>
+            </p>
+        </div>
     </div>
     </>
     );
